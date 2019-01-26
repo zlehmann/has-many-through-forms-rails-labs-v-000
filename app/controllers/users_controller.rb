bccf8c609d53d3da58db68fdbd1f953f65eabed3
Zachary Lehmann <zlehmann@gmail.com>
2019-01-26 23:07:12 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_commented_posts = []
+    @user.comments.each do |comment|
+      @user_commented_posts << comment.post
+    end
   end
 
 end
