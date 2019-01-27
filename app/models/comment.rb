5345d51bf0f52977c898d3180e811e5bccb9b477
Zachary Lehmann <zlehmann@gmail.com>
2019-01-27 21:26:53 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user
+  accepts_nested_attributes_for :user, reject_if: proc { |attr| comments['content'].blank? }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,5 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
-  accepts_nested_attributes_for :comments, reject_if: proc { |attr| comments['content'].blank? }
 
 end
