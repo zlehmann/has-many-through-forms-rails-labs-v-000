89385fbd5c1ccff5129fc338f9faa942ffb1fcae
Zachary Lehmann <zlehmann@gmail.com>
2019-01-28 18:36:45 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,14 @@
 class CommentsController < ApplicationController
 
   def create
-    binding.pry
     comment = Comment.create(comment_params)
+    binding.pry
     redirect_to post_path(Post.find(params[:comment][:post_id]))
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :user, :post_id, :user_id, user_attributes:[:username, :email])
+    params.require(:comment).permit(:content, :post_id, :user_id, user:)
   end
 end
