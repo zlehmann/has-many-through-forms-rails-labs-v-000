2de5704ab6e9faa5b6b7161e921dd3dd82fe1556
Zachary Lehmann <zlehmann@gmail.com>
2019-01-28 20:27:20 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,17 @@
 class CommentsController < ApplicationController
 
   def create
-    binding.pry
     comment = Comment.create(comment_params)
+    if params[:comment][:user_attributes][:username] == ""
+      comment.user = User.find(params[:comment][:user_id])
+      comment.save
+    end
     redirect_to post_path(Post.find(params[:comment][:post_id]))
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :user, :post_id, :user_id, user_attributes:[:username, :email])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes: [:username])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,12 @@ class PostsController < ApplicationController
     @unique_users = []
     @post.comments.each do |comment|
       #binding.pry
-      user = User.find(comment.user_id)
+      user = User.find(comment.user)
       if @unique_users.exclude?(user)
         @unique_users << user
       end
     end
     @unique_users
-
-    @comment = Comment.new
   end
 
   def index
@@ -30,6 +28,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], user_attributes: [:username])
   end
 end
